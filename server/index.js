e27c437d1a63675cdb702b7ab057788f1dec9fe3
ladyshaitan <hello.anne.pruett@gmail.com>
2018-01-27 15:21:34 -0800

    Set up routers for story and event endpoints

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const db = require('../database-mongo');
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 
-app.use('/api/events', require('./event/router'));
-app.use('/api/story', require('./story/story-router'));
+app.use('/api/events', require('./event/event-router'));
+app.use('/api/stories', require('./story/story-router'));
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
