b12ec4b234aac16ef7b898ed233f423e0ec84253
amboz <abozigian@gmail.com>
2018-01-27 14:58:19 -0800

    server file

--- server/index.js
+++ server/index.js
@@ -1,19 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
-// var items = require('../database-mysql');
-// var items = require('../database-mongo');
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('../database-mongo');
 
-var app = express();
+const app = express();
 
-// UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../react-client/dist'));
 
-// UNCOMMENT FOR ANGULAR
-// app.use(express.static(__dirname + '/../angular-client'));
-// app.use(express.static(__dirname + '/../node_modules'));
-
-app.get('/items', function (req, res) {
+app.get('/items', function(req, res) {
   items.selectAll(function(err, data) {
     if(err) {
       res.sendStatus(500);
@@ -26,4 +19,3 @@ app.get('/items', function (req, res) {
 app.listen(3000, function() {
   console.log('listening on port 3000!');
 });
-
