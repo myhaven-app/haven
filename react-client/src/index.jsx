94368523d3dd30214f94800e604a3a1b62c1785c
Janet Lee <janetjhu96@gmail.com>
2018-01-27 15:37:42 -0800

    Resolve merge conflict on server/index.js

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,35 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import List from './components/List.jsx';
+import RegisterUser from './components/RegisterUser.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      items: []
+    this.state = {
+      registrationMode: false
     }
   }
 
-  componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
+  handleCreateUserClick(clickEvent) {
+    (async () => {
+      try {
+        const response = await axios.post('/items', { clickEvent });
+        const data = response.data;
+
+        if (data) {
+          this.setState({
+            tweets: data
+          })
+        }
+      } catch (error) {
+        console.error(error);
       }
-    });
+    })();
   }
 
+
   render () {
+    if (registrationMode) {
+      return (
+        <div>
+          <RegisterUser />
+        </div>
+      )
+    } else {
     return (<div>
       <h1>Item List</h1>
       <List items={this.state.items}/>
     </div>)
+    }
   }
 }
 
