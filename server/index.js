599c9ef3ba57773f81b62dd3d09ac8f50fa15275
ladyshaitan <hello.anne.pruett@gmail.com>
2018-01-27 15:12:08 -0800

    Expose api endpoints

--- server/index.js
+++ server/index.js
@@ -1,20 +1,11 @@
-const express = require('express');
+const app = require('express')();
 const bodyParser = require('body-parser');
 const db = require('../database-mongo');
 
-const app = express();
-
 app.use(express.static(__dirname + '/../react-client/dist'));
 
-app.get('/items', function(req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
-});
+app.use('/api/events', require('./event/router'));
+app.use('/api/story', require('./story/story-router'));
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
